5b8c7807ed4357ff790513216b711bd49bd3271c
Tim Mwangi <44785807+tim-mwangi@users.noreply.github.com>
2020-09-08 13:42:33 -0700

    fix: make ApiNode take in a generic Event type (#6)
    * fix: make ApiNode take in a generic Event type
    
    * Add some unit tests
    
    * address PR comments

--- data-model/src/main/java/org/hypertrace/core/datamodel/shared/ApiNode.java
+++ data-model/src/main/java/org/hypertrace/core/datamodel/shared/ApiNode.java
@@ -3,44 +3,53 @@ package org.hypertrace.core.datamodel.shared;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-import org.hypertrace.core.datamodel.Event;
-import org.hypertrace.core.datamodel.StructuredTrace;
+import org.apache.avro.generic.GenericRecord;
 
-public class ApiNode {
+public class ApiNode<T extends GenericRecord> {
 
-  private final StructuredTrace trace;
-  private final Event entryApiBoundaryEvent;
-  private final List<Event> exitApiBoundaryEvents;
+  private final T entryApiBoundaryEvent;
+  private final List<T> exitApiBoundaryEvents;
+  private final T headEvent;
+  private final List<T> events;
 
-  public ApiNode(StructuredTrace trace, Event apiEntryEvent, List<Event> exitEvents) {
-    this.trace = trace;
+  /**
+   * This creates an API Node(surprise) which contains all the events under an API call within a
+   * single service. The call could be external or internal.
+   * @param headEvent: The event that defines the API call. The head of the API Trace.
+   * @param events: All events under the API call including the head event.
+   * @param apiEntryEvent: The Entry API event. Equals the head event if the head event is an Entry
+   *                     Event. Otherwise it's null.
+   * @param exitEvents: The exit events into another service and API from this API call.
+   */
+  public ApiNode(T headEvent, List<T> events, T apiEntryEvent, List<T> exitEvents) {
     this.entryApiBoundaryEvent = apiEntryEvent;
     this.exitApiBoundaryEvents = exitEvents;
+    this.headEvent = headEvent;
+    this.events = events;
   }
 
   /**
    * Returns the first span in the Api trace. This span could be an API entry or an intermediate
    * service or exit span.
    */
-  public Event getHeadSpan() {
-    Objects.checkIndex(0, trace.getEventList().size());
-    return trace.getEventList().get(0);
-  }
-
-  public StructuredTrace getTrace() {
-    return trace;
+  public T getHeadEvent() {
+    return headEvent;
   }
 
   /**
    * Returns an optional API ENTRY span from this Api node. It's optional because, based on the
    * instrumentation, there could be only Exit call from a service and no Entry.
    */
-  public Optional<Event> getEntryApiBoundaryEvent() {
+  public Optional<T> getEntryApiBoundaryEvent() {
     return Optional.ofNullable(entryApiBoundaryEvent);
   }
 
-  public List<Event> getExitApiBoundaryEvents() {
+  public List<T> getExitApiBoundaryEvents() {
     return exitApiBoundaryEvents;
+  }
+
+  public List<T> getEvents() {
+    return events;
   }
 
   @Override
@@ -52,22 +61,30 @@ public class ApiNode {
       return false;
     }
     ApiNode apiNode = (ApiNode) o;
-    return Objects.equals(trace, apiNode.trace) &&
-        Objects.equals(entryApiBoundaryEvent, apiNode.entryApiBoundaryEvent) &&
-        Objects.equals(exitApiBoundaryEvents, apiNode.exitApiBoundaryEvents);
+    return Objects.equals(entryApiBoundaryEvent, apiNode.entryApiBoundaryEvent) &&
+        Objects.equals(exitApiBoundaryEvents, apiNode.exitApiBoundaryEvents) &&
+        Objects.equals(headEvent, apiNode.headEvent) &&
+        Objects.equals(events, apiNode.events);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(trace, entryApiBoundaryEvent, exitApiBoundaryEvents);
+    return Objects.hash(entryApiBoundaryEvent, exitApiBoundaryEvents, headEvent, events);
   }
 
   @Override
   public String toString() {
-    return "ApiNode{" +
-        "trace=" + trace +
-        ", entryApiBoundaryEvent=" + entryApiBoundaryEvent +
-        ", exitApiBoundaryEvents=" + exitApiBoundaryEvents +
-        '}';
+    StringBuilder sb = new StringBuilder();
+    sb.append("ApiNode{")
+        .append("entryApiBoundaryEvent=")
+        .append(entryApiBoundaryEvent)
+        .append(", exitApiBoundaryEvents=")
+        .append(exitApiBoundaryEvents)
+        .append(", headEvent=")
+        .append(headEvent)
+        .append(", events")
+        .append(events)
+        .append('}');
+    return sb.toString();
   }
 }
