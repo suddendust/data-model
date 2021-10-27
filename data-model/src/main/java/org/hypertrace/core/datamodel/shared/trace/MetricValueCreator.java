6d1786426a3a1039b7857fae7507f3af9ebccaaa
laxman-traceable <93154466+laxman-traceable@users.noreply.github.com>
2021-10-27 14:02:29 +0530

    bugfix: Avro reflection cache (#30)
    * avro builder reflection cache utility added
    
    * avro builder reflection cache utility used in util classes
    
    Co-authored-by: Laxman Ch <laxman@traceable.ai>

--- data-model/src/main/java/org/hypertrace/core/datamodel/shared/trace/MetricValueCreator.java
+++ data-model/src/main/java/org/hypertrace/core/datamodel/shared/trace/MetricValueCreator.java
@@ -1,18 +1,20 @@
 package org.hypertrace.core.datamodel.shared.trace;
 
+import static org.hypertrace.core.datamodel.shared.AvroBuilderCache.fastNewBuilder;
+
 import org.hypertrace.core.datamodel.MetricValue;
 
 public class MetricValueCreator {
 
   public static MetricValue create(double value) {
-    return MetricValue.newBuilder().setValue(value).build();
+    return fastNewBuilder(MetricValue.Builder.class).setValue(value).build();
   }
 
   public static MetricValue create(long value) {
-    return MetricValue.newBuilder().setValue((double) value).build();
+    return fastNewBuilder(MetricValue.Builder.class).setValue((double) value).build();
   }
 
   public static MetricValue create(int value) {
-    return MetricValue.newBuilder().setValue((double) value).build();
+    return fastNewBuilder(MetricValue.Builder.class).setValue((double) value).build();
   }
 }
